e50d00d0cb4caa5ca0a11a6a478f6a5d6f76251f
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2021-11-09 23:11:47 -0300

    create middleware for catController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { LoggerMiddleware } from './modules/common/middleware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './modules/cats/cat.module';
+import { CatController } from './modules/cats/cat.controller';
 import { DatabaseModule } from './modules/database/database.module';
 import { User } from './modules/users/entities/user.entity';
 
@@ -10,4 +17,11 @@ import { User } from './modules/users/entities/user.entity';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'cats', method: RequestMethod.GET }, 'cats/(.*)')
+      .forRoutes(CatController);
+  }
+}
